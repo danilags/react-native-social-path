23654b838233b3e32fd9fdca5d447c9d78fab303
Daniel Sidabutar <aguspermadi94@gmail.com>
2017-05-06 21:45:01 +0700

    succes get location nearby user location

--- socialpath/src/components/App.js
+++ socialpath/src/components/App.js
@@ -38,16 +38,113 @@ import MapsPath from './Maps'
 import ContentHome from './ContentHome'
 
 import MapView from 'react-native-maps';
+import axios from 'axios';
+
+exports.framework = 'React';
+exports.title = 'Geolocation';
+exports.description = 'Examples of using the Geolocation API.';
+
+exports.examples = [
+  {
+    title: 'navigator.geolocation',
+    render: function(): React.Element<any> {
+      return <GeolocationExample />;
+    },
+  }
+];
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      initialPosition: '',
+      lastPosition: {
+        longitude: 0
+      },
+      markers: [],
+      cekinlocation: '',
+      typelocation: ''
+    };
+
+    this.getCurrentMarker = this.getCurrentMarker.bind(this);
 
   }
 
   static navigationOptions = {
     header: null
   };
+
+  watchID: ?number = null;
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var initialPosition = JSON.stringify(position);
+        this.setState({initialPosition});
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lastPosition = position.coords;
+
+      let newState = {
+        lastPositioncustom: {
+          ...lastPosition
+        }
+      }
+      // console.log('Ini lokasi map ---',lastPosition.latitude);
+      this.setState(newState);
+      this.getCurrentMarker(lastPosition.latitude, lastPosition.longitude)
+    });
+  }
+
+  getCurrentMarker(lat, long) {
+    axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=500&key=AIzaSyBVIfTeKhaUqTIWTCa3jvic4zn90EKRqAM`)
+      .then(res => {
+        let customData = res.data.results.map((place) => {
+          console.log('this is location lohh ----',place);
+          return {
+            location: {
+              latitude: place.geometry.location.lat,
+              longitude: place.geometry.location.lng,
+            },
+            name : place.name,
+            type: place.types[0],
+          }
+        })
+        this.setState({
+          markers: customData
+        })
+      })
+
+      .catch(err => {console.log(err)})
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  getInitialState() {
+    return {
+      lastPosition: {
+        latitude: this.state.lastPosition.latitude,
+        longitude: this.state.lastPosition.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  cobaMethod() {
+    return {
+      name : ''
+    }
+  }
+
+  onRegionChange(lastPosition) {
+    this.setState(lastPosition );
+  }
 
   render() {
     return (
@@ -57,20 +154,34 @@ class App extends React.Component {
           <SearchPath />
         </View>
 
-        <ContentHome />
+        <ContentHome location={this.state.cekinlocation} typelocation={this.state.typelocation}/>
 
         <FooterPath navigation={this.props.navigation} />
         <View style={styles.container2}>
         <MapView
-           style={styles.map}
-           initialRegion={{
-             latitude: -6.260507,
-             longitude: 106.781495,
-             latitudeDelta: 0.0922,
-             longitudeDelta: 0.0421,
-           }}
-           showsUserLocation={true}
-          />
+          style={styles.map}
+          region={this.state.region}
+          onRegionChange={this.onRegionChange.bind(this)}
+          showsUserLocation={true}
+          loadingEnabled={true}
+        >
+          {this.state.markers.map((marker, index) => (
+            <MapView.Marker
+              key={index}
+              coordinate={marker.location}
+              title={marker.name}
+              onPress={() => this.setState({
+                cekinlocation: marker.name,
+                typelocation: marker.type
+              })}
+              >
+                <MapView.Callout>
+
+                </MapView.Callout>
+              </MapView.Marker>
+          ))}
+
+        </MapView>
         </View>
       </Container>
     )
